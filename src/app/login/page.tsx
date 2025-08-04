8cc14d75d0265746fc2c73711bc9931c2f1a7b7b
tinhtan2505 <61924615+tinhtan2505@users.noreply.github.com>
2025-08-04 13:21:33 +0700

    Add login page and API service, update dependencies
    Introduced a new login page at src/app/login/page.tsx and an API service utility at src/app/lib/api-service.ts. Updated global styles and layout, and made minor changes to the main page. Also updated and added several dependencies in package.json to support new features and UI components.

---
+++ src/app/login/page.tsx
@@ -0,0 +1,86 @@
+"use client";
+import { Button, Checkbox, Form, Input, message } from "antd";
+import React from "react";
+// import { isAuthenticated, login } from "../../lib/api-service";
+import { useNavigate } from "react-router-dom";
+import { isAuthenticated, login } from "../lib/api-service";
+
+const Login: React.FC<object> = ({}) => {
+  //   const navigate = useNavigate();
+
+  interface LoginFormValues {
+    username: string;
+    password: string;
+    remember?: boolean;
+  }
+
+  const onFinish = async (values: LoginFormValues) => {
+    await login(values.username, values.password);
+    if (isAuthenticated()) {
+      message.success("Đăng nhập thành công!");
+    }
+  };
+
+  //   const onFinishFailed = (errorInfo: any) => {
+  //     console.log("Failed:", errorInfo);
+  //   };
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <div className="bg-white p-8 rounded-lg shadow-md w-96">
+        <h2 className="text-2xl font-semibold">Welcome back!</h2>
+        <h3 className="text-lg mb-6">Log in to TTH Payment Listener</h3>
+        <Form
+          name="login"
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+          //   onFinishFailed={onFinishFailed}
+          layout="vertical"
+        >
+          <Form.Item
+            label="UserName"
+            name="username"
+            rules={[{ required: true, message: "Please input your username!" }]}
+          >
+            <Input placeholder="Type your username address" />
+          </Form.Item>
+
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[{ required: true, message: "Please input your password!" }]}
+          >
+            <Input.Password placeholder="Type your password" />
+          </Form.Item>
+
+          <div className="flex justify-between items-center mb-3">
+            <Form.Item name="remember" valuePropName="checked" noStyle>
+              <Checkbox>Remember me</Checkbox>
+            </Form.Item>
+            <a className="text-red-500" href="">
+              Forgot password?
+            </a>
+          </div>
+
+          <Form.Item>
+            <Button
+              type="primary"
+              htmlType="submit"
+              className="w-full bg-red-500 hover:bg-red-600"
+            >
+              Log in
+            </Button>
+          </Form.Item>
+        </Form>
+
+        <div className="text-center mt-4">
+          <span>Do not have an account? </span>
+          <a className="text-red-500" href="">
+            Sign up
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
